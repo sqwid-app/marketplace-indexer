0f7c82919e4f15f807a1e019c759159c27a8eb9f
boo-0x <boo_0x@tutanota.com>
2023-12-21 08:36:51 +0100

    Update Firebase env vars

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,25 +1,30 @@
 import admin from 'firebase-admin';
-import { Reference } from "firebase-admin/database";
 
 export class FirebaseDB {
     private db: admin.firestore.Firestore;
 
     constructor() {
-        if (!process.env.FIREBASE_PROJECT_ID) throw new Error('Firebase project ID not set in environment');
-        if (!process.env.FIREBASE_CLIENT_EMAIL) throw new Error('Firebase client email not set in environment');
-        if (!process.env.FIREBASE_PK) throw new Error('Firebase private key not set in environment');
-
-        const firebasePk = Buffer.from(process.env.FIREBASE_PK, 'base64').toString('ascii');
+        const network = process.env.NETWORK;
+        if (!network) throw new Error('Network not set in environment');
+        const projectId = process.env.FIREBASE_PROJECT_ID;
+        if (!projectId) throw new Error('Firebase project ID not set in environment');
+        const clientEmail = process.env[`FIREBASE_MARKETPLACE_CLIENT_EMAIL_${network.toUpperCase()}`];
+        if (!clientEmail) throw new Error('Firebase client email not set in environment');
+        const pkBase64 = process.env[`FIREBASE_MARKETPLACE_PK_${network.toUpperCase()}`];
+        if (!pkBase64) throw new Error('Firebase private key not set in environment');
+        const pk = Buffer.from(pkBase64, 'base64').toString('ascii');
 
         admin.initializeApp({ 
             credential: admin.credential.cert({
-                projectId: process.env.FIREBASE_PROJECT_ID,
-                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                privateKey: firebasePk
+                projectId: projectId,
+                clientEmail: clientEmail,
+                privateKey: pk
             })
         });
 
         this.db = admin.firestore();
+        
+        console.log('  Firebase DB initialized\n');
     }
 
     public async notifyEvent(blockHeight: number, events: string[]) {
